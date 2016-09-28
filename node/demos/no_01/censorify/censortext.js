152f07d3ae636ec7a030211033bc695d0a033b85
william@bugwe <william@bugwe.com>
2016-09-28 10:59:15 +0800

    1、创建 consorify

---
+++ node/demos/no_01/censorify/censortext.js
@@ -0,0 +1,20 @@
+ensoreWords = ['sad', 'bad', 'mad'];
+var customCensoredWords = [];
+function censor(inStr) {
+	for (idx in censoreWords) {
+		inStr = inStr.replace(censoreWords[idx], '****');
+	}
+	for (idx in customCensoredWords) {
+		inStr = inStr.replace(customCensoredWords[idx]. '****');
+	}
+	return inStr;
+}
+function addCensoredWord(word) {
+	customCensoredWords.push(word);
+}
+function getCensoredWords() {
+	return censoreWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
